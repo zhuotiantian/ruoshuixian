422e1ee2170e5a401f8be3a4a5536f0ea149da5a
zhuotiantian <2252930124@qq.com>
2019-07-10 18:38:30 +0800

    前端页面调整

---
+++ ruoshuixian/src/pages/hongbao/getCash/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+
+  </div>
+</template>
+<style lang="scss" scoped>
+  .container {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: $grey-background;
+    padding: tovmin(30);
+  }
+
+</style>
