d035eccaed4e096baec59d9d3ac52efef2fdd201
zhuotiantian <2252930124@qq.com>
2019-09-18 16:15:28 +0800

    修改修改

--- ruoshuixian/src/pages/auth/index.vue
+++ ruoshuixian/src/pages/auth/index.vue
@@ -1,90 +1,92 @@
 <template>
-    <div class="wrapper">
-        <div>
-            <image src="/static/images/my/avator.png" width="60" height="60"></image>
-            <p>获取您的公开信息（昵称、头像）等</p>
-            <button class="btn" open-type="getPhoneNumber" @getphonenumber="getPhoneNumber">授权登陆</button>
-        </div>
+  <div class="wrapper">
+    <div>
+      <image src="/static/images/my/avator.png" width="60" height="60"></image>
+      <p>获取您的公开信息（昵称、头像）等</p>
+      <button class="btn" open-type="getPhoneNumber" @getphonenumber="getPhoneNumber">授权登陆</button>
     </div>
+  </div>
 </template>
 <script>
-    export default {
-        onLoad(options) {
-            this.inviter_id = options.inviterid
-        },
-        data() {
-            return {
-                inviter_id: null
-            }
-        },
-        methods: {
-            getPhoneNumber: function(e) {
-                if (e.mp.detail.iv) {
-                    let that = this;
-                    wx.login({
-                        success: function(res) {
-                            that.$setStorage("code", res.code).then(() => {
-                                let params = {
-                                    rawData: JSON.stringify(e.mp.detail),
-                                    code: res.code,
-                                    type: "user",
-                                };
-                                if (that.inviter_id && that.inviter_id !== "null") {
-                                    params.inviter_id = that.inviter_id;
-                                }
-                                that.$http.post({
-                                    url: "/api/wxapp.user/login",
-                                    data: params
-                                }).then(result => {
-                                    if (result.code == 1) {
-                                        that.$setStorage("userInfo", result.data.userInfo).then(() => {
-                                            wx.switchTab({
-                                                url: "/pages/firstPage/main"
-                                            })
-                                        });
-                                    }
-                                });
-                            })
-                        }
-                    });
+export default {
+  onLoad (options) {
+    this.inviter_id = options.inviterid
+    this.gameid = options.gameid;
+  },
+  data () {
+    return {
+      inviter_id: null,
+      gameid: null
+    }
+  },
+  methods: {
+    getPhoneNumber: function (e) {
+      if (e.mp.detail.iv) {
+        let that = this;
+        wx.login({
+          success: function (res) {
+            that.$setStorage("code", res.code).then(() => {
+              let params = {
+                rawData: JSON.stringify(e.mp.detail),
+                code: res.code,
+                type: "user",
+              };
+              if (that.inviter_id && that.inviter_id !== "null") {
+                params.inviter_id = that.inviter_id;
+              }
+              that.$http.post({
+                url: "/api/wxapp.user/login",
+                data: params
+              }).then(result => {
+                if (result.code == 1) {
+                  that.$setStorage("userInfo", result.data.userInfo).then(() => {
+                    wx.switchTab({
+                      url: "/pages/firstPage/main?gameid=" + this.gameid
+                    })
+                  });
                 }
-            }
-        }
-    };
+              });
+            })
+          }
+        });
+      }
+    }
+  }
+};
 </script>
 <style scoped lang="scss">
-    .wrapper {
-        width: 100%;
-        height: 100%;
-        background: #eaeaea !important;
-        position: absolute;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+.wrapper {
+  width: 100%;
+  height: 100%;
+  background: #eaeaea !important;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-    image {
-        width: tovmin(120);
-        height: tovmin(120);
-        border-radius: 50%;
-    }
+image {
+  width: tovmin(120);
+  height: tovmin(120);
+  border-radius: 50%;
+}
 
-    .wrapper>div {
-        text-align: center;
-    }
+.wrapper > div {
+  text-align: center;
+}
 
-    image,
-    p,
-    button {
-        margin-bottom: tovmin(30);
-    }
+image,
+p,
+button {
+  margin-bottom: tovmin(30);
+}
 
-    .btn {
-        width: tovmin(300);
-        height: tovmin(70);
-        line-height: tovmin(70);
-        border-radius: tovmin(8);
-        background: $deep-blue;
-        color: white;
-    }
+.btn {
+  width: tovmin(300);
+  height: tovmin(70);
+  line-height: tovmin(70);
+  border-radius: tovmin(8);
+  background: $deep-blue;
+  color: white;
+}
 </style>
